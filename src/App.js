10dbfd5ad9ef082b7ee759823ae43b13a87a7148
Mohit132 <mohitdubey1322001@gmil.com>
2023-07-22 21:07:02 +0530

    fun

--- src/App.js
+++ src/App.js
@@ -2,75 +2,7 @@ import React, { Component, Suspense } from 'react';
 import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n.js'
-// import logo from './logo.svg';
-// import './App.css';
-
-// use hoc for class based components
-// class LegacyWelcomeClass extends Component {
-//   render() {
-//     const { t } = this.props;
-//     return <h2>{t('title')}</h2>;
-//   }
-// }
-// const Welcome = withTranslation()(LegacyWelcomeClass);
-
-// // Component using the Trans component
-// function MyComponent() {
-//   return (
-//     <Trans i18nKey="description.part1">
-//       To get started, edit <code>src/App.js</code> and save to reload.
-//     </Trans>
-//   );
-// }
-
-// // page uses the hook
-// function Page() {
-//   const { t, i18n } = useTranslation();
-
-  // const changeLanguage3 = (lng) => {
-  //   // i18n.changeLanguage(lng);
-  //   i18n.changeLanguage(lng)
-  // };
-
-  // return (
-  //   <div className="App">
-  //     <div className="App-header">
-  //       {/* <img src={logo} className="App-logo" alt="logo" /> */}
-  //       <Welcome />
-  //       <button type="button" onClick={() => changeLanguage3('ja')}>
-  //         ja
-  //       </button>
-  //       <button type="button" onClick={() => changeLanguage3('en')}>
-  //         en
-  //       </button>
-  //       <button type="button" onClick={() => changeLanguage3('th')}>
-  //         thai
-  //       </button>
-  //     </div>
-  //     <div className="App-intro">
-  //       <MyComponent />
-  //     </div>
-  //     <div>{t('description.part2')}</div>
-  //   </div>
-  // );
-// }
-
-// // loading component for suspense fallback
-// const Loader = () => (
-//   <div className="App">
-//     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//     <div>loading...</div>
-//   </div>
-// );
-
-// // here app catches the suspense from page in case translations are not yet loaded
-// export default function App() {
-//   return (
-//     <Suspense fallback={<Loader />}>
-//       <Page />
-//     </Suspense>
-//   );
-// } 
+import './App.css';
 function App (){
  const {t,i18n} =useTranslation( );
  const changeLanguage3 = (lng) => {
@@ -83,9 +15,33 @@ return (
 <I18nextProvider i18n={i18n}>
 <Suspense fallback="dddddddddd">
     <div className="App">
-      <div className="App-header" style={{display:"flex" ,margin:"5rem",alignItems:"center",justifyContent:"center"}}>
+      <div className="App-header" style={{display:"flex" ,alignItems:"center",justifyContent:"center"}}>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <button type="button" onClick={() => changeLanguage3('ja')}>
+      </div>
+      <div className="app">
+      {/* Header */}
+      <header className="header">
+        <nav>
+          <h1>Your Logo</h1>
+          <ul>
+            <li>{t('nav.home')}</li>
+            <li>{t('nav.about')}</li>
+            <li>{t('nav.connect')}</li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Hero Section */}
+      <section className="hero">
+        <div className="hero-content">
+          <h2>{t('hero.headline')}</h2>
+          <p>
+          {t('hero.details')}
+          </p>
+          <button>{t('hero.callOfaction')}</button>
+        </div>
+      </section>
+      <button type="button" onClick={() => changeLanguage3('ja')}>
           ja
         </button>
         <button type="button" onClick={() => changeLanguage3('en')}>
@@ -94,15 +50,14 @@ return (
         <button type="button" onClick={() => changeLanguage3('th')}>
           thai
         </button>
-      </div>
-      <div className="App-intro">
-        {/* <MyComponent /> */}
-      </div>
-      <div style={{alignItems:"center",display:"flex",flexDirection:"column",padding:"auto",margin:"auto"}}>
-      <div>{t('description.part2')}</div>
-      <div>{t('title')}</div>
-      <div>{t('description.part1')}</div>
-      </div>
+    
+      {/* Footer */}
+      <footer className="footer">
+        <p>{t('footer')} </p>
+      </footer>
+    </div>
+
+     
       {/* <div>{t('description.part2')}</div> */}
     </div>
   
--- src/i18n.js
+++ src/i18n.js
@@ -4,36 +4,55 @@ import { initReactI18next } from 'react-i18next';
 const resources = {
   en: {
     translation: {
-      greeting: 'Hello!',
-      title: "Welcome to react using react-i18next",
-      description: {
-        part1: "To get started, edit <1>src/App.js</1> and save to reload.",
-        part2: "Switch language between english and german using buttons above."
+     nav:{
+      home:"Home",
+      about:"About",
+      connect:"Connect"
+     },
+     hero:{
+      headline:"Welcome to our Amazing Product!",
+      details:" Are you tired of searching for the perfect solution to your everyday problems? Look no further! Our Amazing Product is here to save the day!",
+      callOfaction:"Get Started Now"
+     },
+     footer:"&copy;12 march 2024 Your Company. All rights reserved."
       }
       // ...other translations
     },
-  },
   ja: {
     translation: {
-      title: "react-i18nextを使用したReactへようこそ",
-  description: {
-    part1: "始めるには、<1>src/App.js</1>を編集して保存してリロードしてください。",
-    part2: "上のボタンを使って英語とドイツ語の間で言語を切り替えます。"
-  }
-    }
+     nav:{
+    home: "ホーム",
+    about: "について",
+    connect: "お問い合わせ"
   },
+  hero:{
+    headline: "素晴らしい製品へようこそ！",
+    details: "日々の問題の完璧な解決策を探すのに疲れましたか？心配しないで！私たちの素晴らしい製品がお手伝いします！",
+    callOfaction: "今すぐ始めましょう"
+  },
+  footer:"&copy; 2024年3月12日 あなたの会社。全著作権所有。"
+    },
+  },
+
   th:{
     translation:{
-      title: "ยินดีต้อนรับสู่ React ด้วย react-i18next",
-      description: {
-        part1: "เพื่อเริ่มต้น แก้ไข <1>src/App.js</1> และบันทึกเพื่อรีโหลด",
-        part2: "สลับภาษาระหว่างอังกฤษและเยอรมันโดยใช้ปุ่มด้านบน"
-      }
+    
+  nav: {
+    home: "หน้าหลัก",
+    about: "เกี่ยวกับ",
+    connect: "ติดต่อ"
+  },
+  hero: {
+    headline: "ยินดีต้อนรับสู่ผลิตภัณฑ์ที่น่าทึ่งของเรา!",
+    details: "คุณเบื่อหน่ายหาทางแก้ไขปัญหาที่สวยงามให้กับปัจจุบันหรือยัง? ไม่ต้องห่วง! ผลิตภัณฑ์ที่น่าทึ่งของเรามาเพื่อช่วยคุณ!",
+    callOfaction: "เริ่มต้นใช้งานเดี๋ยวนี้"
+  },
+  footer: "&copy; 12 มีนาคม 2024 บริษัทคุณ สงวนลิขสิทธิ์ทั้งหมด"  }
     }
-  }
+  };
 
   // ...other languages
-};
+
 
 // Initialize i18next
 i18n.use(initReactI18next).init({
